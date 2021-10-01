f7453f16feddbf59ef6af3b47f4cb2b10febfacd
승철 차 <cscculture95@gmail.com>
2021-10-01 11:40:14 +0900

    벨만포드 알고리즘 이용해서 경유지 들리는 항공권 알고리즘 구현

--- leetcode/최단 경로 문제/743/sol.py
+++ leetcode/최단 경로 문제/743/sol.py
@@ -1,32 +1,13 @@
 class Solution:
-    def networkDelayTime(self, times: List[List[int]], n: int, k: int) -> int:
-        graph = collections.defaultdict(list)
-        
-        for u, v, w in times : 
-            graph[u].append((v, w))
-            
-        # 소요시간, 정점
-        # 즉 시작점에서 정점까지 거리
-        # 처음에는 k->k니까 0이다.
-        Q = [(0, k)]
-        dist = collections.defaultdict(int)
-        
-        while Q : 
-            time, node = heapq.heappop(Q)
-            # 만약 dist에 정점이 없다면
-            if node not in dist : 
-                # node의 dist로 time을 넣어준다.
-                dist[node] = time
-                # 모든 edge를 순회하며
-                for v, w in graph[node] : 
-                    #  총 시간을 구한다.
-                    alt = time + w
-                    # Q에 시간과 시작 vertex를 갱신한다.
-                    heapq.heappush(Q, (alt, v))
-                    
-                    
-        # 모든 노드의 최단 경로 존재를 판별한다.
-        if len(dist) == n : 
-            return max(dist.values())
-        return -1
-                    
+    def findCheapestPrice(self, n: int, flights: List[List[int]], src: int, dst: int, K: int) -> int:
+        INFINITY : int = 2147483647
+        cost = [INFINITY for _ in range(n)]
+        cost[src] = 0
+
+        for i in range(K + 1):
+            tmp = list(cost)
+            for p in flights:
+                tmp[p[1]] = min(tmp[p[1]], cost[p[0]] + p[2])
+            cost = tmp
+
+        return -1 if cost[dst] >= 1e9 else cost[dst]
