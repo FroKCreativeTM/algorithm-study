6669a3c86723daf88c5a86c2434c9924e6e518d6
frokcreative <cscculture95@gmail.com>
2021-10-14 11:05:00 +0900

    메모이제이션을 이용한 피보나치 수열

---
+++ leetcode/DP/509/sol.py
@@ -0,0 +1,13 @@
+class Solution:
+    dp = collections.defaultdict(list)
+    def fib(self, n: int) -> int:
+        if n <= 1 : 
+            return n
+        
+        if self.dp[n] : 
+            return self.dp[n]
+        
+        self.dp[n] = self.fib(n - 1) + self.fib(n - 2)
+        return self.dp[n]
+        
+        
