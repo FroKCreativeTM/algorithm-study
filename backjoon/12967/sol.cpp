0a9a8a1b6151c0325b150d1111c985d0a0705e77
승철 차 <cscculture95@gmail.com>
2021-11-07 23:31:58 +0900

    12967번 풀이중

---
+++ backjoon/12967/sol.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+long long N, K;
+vector<long long> vec;
+int cnt = 0;
+
+void solve() {
+    for(int p = 0; p < vec.size() - 2; ++p) {
+        for(int q = 1; q < vec.size() - 1; ++q) {
+            for(int r = 2; r < vec.size(); ++r) {
+                if(p == q || q == r || r == p) {
+                    continue;
+                }
+
+                if((vec[p] * vec[q] * vec[r]) % K == 0) {
+                    cnt++;
+                }
+            }
+        }
+    }
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    cin >> N >> K;
+    
+    for (int i = 0; i < N; ++i)
+    {
+        long long tmp;
+        cin >> tmp;
+        vec.push_back(tmp);
+    }
+    
+    solve();
+
+    cout << cnt << endl;
+}
